fab606010b6e0606702af33812dac0719c555840
FlutterX3 <se57160095@gmail.com>
2019-04-29 14:10:26 +0700

    Create method

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,8 +6,32 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+// ---------------------------
+  Widget showName() {
+    return Text('Fluter');
+  }
+
+// -------------------------------------------
   @override
   Widget build(BuildContext context) {
-    return Text(' This is two ');
+    return Scaffold(
+        body: Container(
+      padding: EdgeInsets.only(top: 30.0),
+      alignment: Alignment(0, -1),
+      child: showName(),
+    ));
+// ----------------------
+    // return Container(
+    //   decoration: BoxDecoration(color: Colors.white),
+    //   child: Center(
+    //     child: Text(
+    //       'This is  World',
+    //       style: TextStyle(
+    //         fontSize: 32,
+    //         color: Colors.black87,
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
